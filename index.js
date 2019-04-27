10982cc19f293410fd19870e42aa5899f7dbeb98
Anais Trope <trope.anais@gmail.com>
2019-04-27 14:05:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,16 +22,6 @@ logWhisper("HELLO!")
 
 //sayHiToGrandma(string) returns "I can't hear you!" if `string` is lowercase
 
-var uppercase = 'HELLO'
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello'
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
-mixedCase.toLowerCase() === mixedCase // false
-mixedCase.toUpperCase() === mixedCase // false
-
 function sayHiToGrandma(string){
   if (lowercase) { 
     return("I can't hear you!")}
