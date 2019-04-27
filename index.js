4a16065f8ade37c167c37ab4a6e9f10f2a467670
Anais Trope <trope.anais@gmail.com>
2019-04-27 14:32:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,10 +46,8 @@ function sayHiToGrandma(string){
     return ("I can't hear you!")
   } else if (string === string.toUpperCase()){
     return ("YES INDEED!")
-  } else 
-    
-  }
-    
+  } else {
+    return ("I love you, too.")}
 }
 
 sayHiToGrandma("I love you, Grandma.")
